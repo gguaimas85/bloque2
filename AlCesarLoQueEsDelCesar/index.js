d83b2a74bfc2e427fa56b5aa76ab36dfdd477e9a
Gustavo Guaimas <55361033+gguaimas85@users.noreply.github.com>
2020-11-28 01:15:10 -0300

    Challenge Codigo del Cesar

---
+++ AlCesarLoQueEsDelCesar/index.js
@@ -0,0 +1,22 @@
+//[AlCesarLoQueEsDelCesar] Implementar una función que pueda aplicar el cifrado del césar. (Googlear)
+
+const caesarCode = (texto, desp) =>{
+    const cifrado=[];
+    let codigoLetra;
+
+    for (let i = 0; i < texto.length; i++) {
+        if (texto[i]==" ") {
+            cifrado[i]=texto[i] ;
+        }else{
+            codigoLetra = texto.charCodeAt(i)+desp;
+            codigoLetra>122 ? codigoLetra=codigoLetra-26: null;
+            codigoLetra>90 && codigoLetra<97 ? codigoLetra=codigoLetra-26 :null;
+            cifrado[i]=String.fromCharCode(codigoLetra);
+
+        } 
+        
+    }
+
+    return cifrado.join('');
+
+}
