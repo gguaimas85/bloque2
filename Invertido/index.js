be7b63d27080d37762effe27d5137abb0f3d9a2c
Gustavo Guaimas <55361033+gguaimas85@users.noreply.github.com>
2020-11-28 14:10:08 -0300

    Challenge revertir string

---
+++ Invertido/index.js
@@ -0,0 +1,12 @@
+//Armar una función que reciba como parámetro un string y lo retorne invertido
+
+const reverse =(string) =>{
+    const array =[];
+
+    for (let i = str.length; i >= 0; i--) {
+        array.push(str[i]);
+        
+    }
+
+    return array.join('');
+}
