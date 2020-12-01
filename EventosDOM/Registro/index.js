e86501b8726cec254457a93272bb497458771aed
Gustavo Guaimas <55361033+gguaimas85@users.noreply.github.com>
2020-12-01 11:20:12 -0300

    Challenge Registro

--- EventosDOM/Registro/index.js
+++ EventosDOM/Registro/index.js
@@ -1,2 +1,20 @@
 //Cargar el nombre y apellido dentro de dos inputs type ="text" y al apretar un boton mostrar la concatenacion de estos strings en un tercer input de tipo text.
 
+
+let nom="";
+let ape="";
+let completo="";
+
+
+const handlerInputText = (e) =>{
+    console.log(e.value);
+    e.id=="nombre" ? nom=e.value: null;
+    e.id=="apellido" ? ape=e.value: null;
+   
+}
+
+const nombreApellido = () =>{
+    completo = nom + " " + ape;
+    
+    document.getElementById("nombreCompleto").value=completo;
+}
